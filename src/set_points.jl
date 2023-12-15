2e9dd876cb57761220aff9f5f406b736cef7a855
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2023-12-15 11:13:58 +0100

    Add timers in set_points!

--- src/set_points.jl
+++ src/set_points.jl
@@ -10,17 +10,24 @@ end
 # Here the element type of `xp` can be either an NTuple{N, <:Real}, an SVector{N, <:Real},
 # or anything else which has length `N`.
 function set_points!(p::PlanNUFFT{T, N}, xp::AbstractVector) where {T, N}
-    (; points,) = p
+    (; points, timer,) = p
     type_length(eltype(xp)) == N || throw(DimensionMismatch(lazy"expected $N-dimensional points"))
-    resize!(points, length(xp))
-    Base.require_one_based_indexing(points)
-    @inbounds for (i, x) ∈ enumerate(xp)
-        points[i] = to_unit_cell(NTuple{N}(x))  # converts `x` to Tuple if it's an SVector
+    @timeit timer "Set points" begin
+        @timeit timer "Copy/fold" begin
+            resize!(points, length(xp))
+            Base.require_one_based_indexing(points)
+            @inbounds for (i, x) ∈ enumerate(xp)
+                points[i] = to_unit_cell(NTuple{N}(x))  # converts `x` to Tuple if it's an SVector
+            end
+        end
+        @timeit timer "Sort" begin
+            sort_points!(p.blocks, points)
+        end
     end
-    sort_points!(p.blocks, points)
     p
 end
 
+# "Folds" location onto unit cell [0, 2π]ᵈ.
 to_unit_cell(x⃗) = map(_to_unit_cell, x⃗)
 
 function _to_unit_cell(x::Real)
