122f4c23a0dac6279f2f7e6571ec5032214f38f9
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-06-25 09:10:08 +0200

    Fix round-off issue when x = π - ε

--- test/near_2pi.jl
+++ test/near_2pi.jl
@@ -1,10 +1,19 @@
 using Test
 using NonuniformFFTs
 using NonuniformFFTs: Kernels
-using FFTW: fftfreq
+using FFTW: fftfreq, rfftfreq
+
+function type1_exact!(us, ks, xp, vp)
+    fill!(us, 0)
+    for i ∈ eachindex(xp, vp)
+        for j ∈ eachindex(us)
+            us[j] += vp[i] * cis(-xp[i] * ks[j])
+        end
+    end
+    us
+end
 
 # Test that roundoff errors are properly accounted for when a point is very close to 2π.
-
 @testset "Point near 2π" begin
     L = 2π
 
@@ -22,13 +31,9 @@ using FFTW: fftfreq
         us = Array{T}(undef, N)
         exec_type1!(us, plan, vp)
 
-        us_exact = fill!(similar(us), 0)
+        us_exact = similar(us)
         ks = fftfreq(N, N)
-        for i ∈ eachindex(xp, vp)
-            for j ∈ eachindex(us_exact)
-                us_exact[j] += vp[i] * cis(-xp[i] * ks[j])
-            end
-        end
+        type1_exact!(us_exact, ks, xp, vp)
 
         @test isapprox(us, us_exact; rtol = 1e-11)
     end
@@ -45,4 +50,31 @@ using FFTW: fftfreq
             @test Kernels.point_to_cell(x, Δx) == 3
         end
     end
+end
+
+# Test similar issue when x = π - ε
+@testset "Point near π" begin
+    N = 16  # number of Fourier modes
+    T = Float64
+    x = T(π)
+    x = prevfloat(x)
+    xp = [x]
+    vp = [3.4]
+
+    @testset "Kernels.point_to_cell" begin
+        Δx = T(2π) / 24
+        i = Kernels.point_to_cell(x, Δx)
+        @test (i - 1) * Δx ≤ x < i * Δx
+    end
+
+    plan_nufft = PlanNUFFT(T, N; m = HalfSupport(4), σ = 1.5)
+    set_points!(plan_nufft, xp)
+    us = Array{Complex{T}}(undef, size(plan_nufft))
+    exec_type1!(us, plan_nufft, vp)
+
+    us_exact = similar(us)
+    ks = rfftfreq(N, N)
+    type1_exact!(us_exact, ks, xp, vp)
+
+    @test isapprox(us, us_exact; rtol = 1e-5)
 end
