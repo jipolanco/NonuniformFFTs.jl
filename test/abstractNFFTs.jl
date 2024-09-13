b4ed8cb4a1f1ce8ecc3e83e0cabac44bfc8595cf
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-09-13 13:04:55 +0200

    Set random seed in test

--- test/abstractNFFTs.jl
+++ test/abstractNFFTs.jl
@@ -1,16 +1,18 @@
 # Test the AbstractNFFTs.jl interface and compare results with NFFT.jl.
 
 using Test
+using Random: Random
 using NonuniformFFTs
 using NFFT: NFFT, size_in, size_out
 
 Ns = (64, 81)  # number of Fourier modes in each direction (test odd sizes as well)
 Np = 1000      # number of non-uniform points
 
+rng = Random.Xoshiro(43)
 T = Float64                      # must be a real data type (Float32, Float64)
 d = length(Ns)                   # number of dimensions (d = 2 here)
-xp = rand(T, (d, Np)) .- T(0.5)  # non-uniform points in [-1/2, 1/2)ᵈ; must be given as a (d, Np) matrix
-vp = randn(Complex{T}, Np)       # random values at points (must be complex)
+xp = rand(rng, T, (d, Np)) .- T(0.5)  # non-uniform points in [-1/2, 1/2)ᵈ; must be given as a (d, Np) matrix
+vp = randn(rng, Complex{T}, Np)       # random values at points (must be complex)
 
 reltol = 1e-9
 window = :kaiser_bessel
