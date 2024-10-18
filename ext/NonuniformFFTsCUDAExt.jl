008a5e9b5e3779fd503b5cc022bd456b683cddcf
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2024-10-18 15:44:11 +0200

    Avoid Atomix on CUDA

--- ext/NonuniformFFTsCUDAExt.jl
+++ ext/NonuniformFFTsCUDAExt.jl
@@ -3,4 +3,18 @@ module NonuniformFFTsCUDAExt
 using NonuniformFFTs
 using CUDA
 
+@inline function NonuniformFFTs._atomic_add!(u::CuDeviceArray{T}, v::T, inds::Tuple) where {T <: Real}
+    @inbounds CUDA.@atomic u[inds...] += v
+end
+
+@inline function NonuniformFFTs._atomic_add!(u::CuDeviceArray{T}, v::Complex{T}, inds::Tuple) where {T <: Real}
+    @inbounds begin
+        i₁ = 2 * (inds[1] - 1)  # convert from logical index (equivalent complex array) to memory index (real array)
+        itail = Base.tail(inds)
+        CUDA.@atomic u[i₁ + 1, itail...] += real(v)
+        CUDA.@atomic u[i₁ + 2, itail...] += imag(v)
+    end
+    nothing
+end
+
 end
