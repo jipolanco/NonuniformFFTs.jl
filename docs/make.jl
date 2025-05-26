d0676f50d46469111901830adcd1606bb9e1cc7e
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2025-05-26 09:26:47 +0200

    Rename benchmarks -> benchmark dir

--- docs/make.jl
+++ docs/make.jl
@@ -4,7 +4,7 @@ using Downloads: Downloads
 using NonuniformFFTs
 
 # Copy benchmark results to docs/src/benchmarks/
-srcdir = relpath(joinpath(@__DIR__, "..", "benchmarks", "plots"))
+srcdir = relpath(joinpath(@__DIR__, "..", "benchmark", "plots"))
 dstdir = relpath(joinpath(@__DIR__, "src", "img"))
 mkpath(dstdir)
 for fname ∈ readdir(srcdir)
