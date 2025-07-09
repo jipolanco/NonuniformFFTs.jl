3fa0b4a77eebf1fbbe8ee01c4abef9c7cd753476
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2025-07-09 11:39:02 +0200

    Add AMDGPU benchmark script + update CUDA one

--- benchmark/CPU+AMDGPU/run_benchmarks.jl
+++ benchmark/CPU+AMDGPU/run_benchmarks.jl
@@ -1,7 +1,7 @@
 using NonuniformFFTs
 using FINUFFT
 using Adapt
-using CUDA
+using AMDGPU
 using FFTW
 using LinearAlgebra: norm
 using KernelAbstractions
@@ -20,22 +20,20 @@ end
 
 threadinfo()
 
-CUDA.versioninfo()
-@show CUDA.device()
+AMDGPU.versioninfo()
 
-# # Useful for FINUFFT (CPU):
+# Useful for FINUFFT (CPU):
 # ENV["OMP_PROC_BIND"] = "true"
 # # ENV["OMP_PLACES"] = "cores"
 # ENV["OMP_NUM_THREADS"] = Threads.nthreads()
 
 get_device_name(::CPU) = string(Sys.cpu_info()[1].model, " ($(Sys.CPU_NAME), $(Sys.CPU_THREADS) threads)")
 
-function get_device_name(::CUDABackend)
-    dev = CUDA.device()
-    io = IOBuffer()
-    show(io, MIME"text/plain"(), dev)  # e.g. "CuDevice(1): NVIDIA A100 80GB PCIe"
-    s = String(take!(io))
-    split(s, " "; limit = 2)[2]
+function get_device_name(::ROCBackend)
+    dev = AMDGPU.device()
+    name = AMDGPU.HIP.name(dev)  # e.g. "AMD Instinct MI300A"
+    gcn = AMDGPU.HIP.gcn_arch(dev)  # e.g. "gfx942:sramecc+:xnack-"
+    "$name ($gcn)"
 end
 
 function bench_nonuniformffts(
@@ -121,71 +119,6 @@ function bench_finufft_cpu(::Type{Z}, Ns::Dims, Np; tol, opts...) where {Z <: Co
     (; type1,)
 end
 
-function bench_cufinufft(::Type{Z}, Ns::Dims, Np; tol, opts...) where {Z <: Complex}
-    backend = CUDABackend()
-    T = real(Z)
-    D = length(Ns)
-    rng = Xoshiro(42)
-    xp = ntuple(D) do _
-        adapt(backend, randn(rng, T, Np))
-    end
-    vp = adapt(backend, randn(rng, Z, Np))
-    wp = similar(vp)
-    ûs = similar(vp, complex(T), Ns)
-    ntrans = 1
-    n_modes = collect(Ns)
-
-    plan_type1 = cufinufft_makeplan(1, n_modes, -1, ntrans, tol; dtype = T, opts...)
-    plan_type2 = cufinufft_makeplan(2, n_modes, +1, ntrans, tol; dtype = T, opts...)
-    finalizer(cufinufft_destroy!, plan_type1)
-    finalizer(cufinufft_destroy!, plan_type2)
-
-    # Execute plans once
-    cufinufft_setpts!(plan_type1, xp...)
-    cufinufft_setpts!(plan_type2, xp...)
-    cufinufft_exec!(plan_type1, vp, ûs)
-    cufinufft_exec!(plan_type2, ûs, wp)
-
-    # Compare with reference case (high accuracy: rtol = 1e-14)
-    relative_errors = let tol = 1e-14
-        p1_ref = cufinufft_makeplan(1, n_modes, -1, ntrans, tol; dtype = T, opts..., gpu_method = 1, upsampfac = 2.0)
-        p2_ref = cufinufft_makeplan(2, n_modes, +1, ntrans, tol; dtype = T, opts..., gpu_method = 1, upsampfac = 2.0)
-
-        ûs_ref = similar(ûs)
-        wp_ref = similar(wp)
-
-        cufinufft_setpts!(p1_ref, xp...)
-        cufinufft_setpts!(p2_ref, xp...)
-        cufinufft_exec!(p1_ref, vp, ûs_ref)
-        cufinufft_exec!(p2_ref, ûs_ref, wp_ref)
-
-        cufinufft_destroy!(p1_ref)
-        cufinufft_destroy!(p2_ref)
-
-        local type1 = norm(ûs_ref - ûs) / norm(ûs_ref)
-        local type2 = norm(wp_ref - wp) / norm(wp_ref)
-
-        KA.unsafe_free!(ûs_ref)
-        KA.unsafe_free!(wp_ref)
-
-        (; type1, type2,)
-    end
-
-    type1 = @benchmark let
-        cufinufft_setpts!($plan_type1, $xp...)
-        cufinufft_exec!($plan_type1, $vp, $ûs)
-        KA.synchronize($backend)
-    end
-
-    type2 = @benchmark let
-        cufinufft_setpts!($plan_type2, $xp...)
-        cufinufft_exec!($plan_type2, $ûs, $wp)
-        KA.synchronize($backend)
-    end
-
-    (; type1, type2, relative_errors,)
-end
-
 function bench_finufft_cpu(::Type{Z}, Ns::Dims, Np; tol, opts...) where {Z <: Complex}
     T = real(Z)
     D = length(Ns)
@@ -255,7 +188,7 @@ function run_benchmark_nonuniformffts(
         fftw_flags = FFTW.ESTIMATE,
 	    kernel = NonuniformFFTs.default_kernel(backend),
 	    kernel_evalmode = NonuniformFFTs.default_kernel_evalmode(backend),
-        use_atomics = false,
+	    use_atomics = false,
         params...,
     ) where {Z <: Number}
     densities = Nps ./ prod(Ns)
@@ -290,52 +223,6 @@ function run_benchmark_nonuniformffts(
         println(io, "# (1) Number of points  (2) Type 1 (median, s)  (3) Type 2 (median, s)  (4) Relative error type 1  (5) Relative error type 2")
         for Np ∈ Nps
             bench = bench_nonuniformffts(Z, backend, Ns, Np; params..., kernel, kernel_evalmode, use_atomics, σ, m, gpu_method, fftw_flags)
-            type1 = median(bench.type1.times) / 1e9  # in seconds
-            type2 = median(bench.type2.times) / 1e9  # in seconds
-            @show Np, type1, type2, bench.relative_errors.type1, bench.relative_errors.type2
-            join(io, (Np, type1, type2, bench.relative_errors.type1, bench.relative_errors.type2), '\t')
-            print(io, '\n')
-            flush(io)
-        end
-    end
-    filename
-end
-
-function run_benchmark_cufinufft(
-        ::Type{Z},
-        Ns::Dims,
-        Nps::AbstractVector;  # number of points per test case
-        tol = 1e-6,
-        modeord = 1,     # convention used by default in NonuniformFFTs
-        gpu_method = 1,  # 1: global memory (-sort) method | 2: shared memory | method 0 or 2 is much slower, at least with Float64 data
-        gpu_sort = 1,    # needed for good performance
-        gpu_kerevalmeth = 1,  # setting it to 0 (direct evaluation) doesn't make a big difference?
-        params...,
-    ) where {Z <: Complex}
-    N = first(Ns)
-    method = gpu_method == 1 ? :global_memory : :shared_memory
-    # Make sure we use the current CUDA device and stream (otherwise KA.synchronize won't
-    # work as expected).
-    params_cuda = (
-        gpu_device_id = CUDA.deviceid(CUDA.device()),
-        gpu_stream = Base.unsafe_convert(Ptr{CUDA.CUstream_st}, CUDA.stream()),
-    )
-    device_name = get_device_name(CUDABackend())
-    filename = "CuFINUFFT_$(N)_$(Z)_$(method).dat"
-    open(filename, "w") do io
-        println(io, "# CuFINUFFT")
-        println(io, "# Benchmark: NUFFT of scalar data")
-        println(io, "#  - Device: ", device_name)
-        println(io, "#  - Element type: ", Z)
-        println(io, "#  - Grid size: ", Ns)
-        println(io, "#  - Relative tolerance: ", tol)
-        println(io, "#  - GPU sort: ", gpu_sort)
-        println(io, "#  - GPU kernel evaluation method: ", gpu_kerevalmeth)
-        println(io, "#  - Order of Fourier modes (modeord): ", modeord)
-        println(io, "#  - GPU method: ", gpu_method, " ($method)")
-        println(io, "# (1) Number of points  (2) Type 1 (median, s)  (3) Type 2 (median, s)  (4) Relative error type 1  (5) Relative error type 2")
-        for Np ∈ Nps
-            bench = bench_cufinufft(Z, Ns, Np; params..., params_cuda..., tol, modeord, gpu_method, gpu_sort, gpu_kerevalmeth)
             type1 = median(bench.type1.times) / 1e9  # in seconds
             type2 = median(bench.type2.times) / 1e9  # in seconds
             @show Np, type1, type2, bench.relative_errors.type1, bench.relative_errors.type2
@@ -403,10 +290,20 @@ function run_all_benchmarks(; cpu = true, gpu = true,)
         round(Int, ρ * Nprod)
     end
 
-    gpu_backend = CUDABackend()
+    gpu_backend = ROCBackend()
 
     @show get_device_name(CPU())
     @show get_device_name(gpu_backend)
+
+    # kernel = BackwardsKaiserBesselKernel()
+    # kernel = KaiserBesselKernel()
+    # kernel_evalmode = Direct()
+    # kernel_evalmode = FastApproximation()
+    params = (;
+        # kernel,
+        # kernel_evalmode,
+        σ, m,
+    )
 
     # Real data
     if gpu
@@ -448,25 +345,6 @@ function run_all_benchmarks(; cpu = true, gpu = true,)
         @info "Running benchmark" Z=Z backend=CPU() use_atomics=true
         flush(stdout)
         run_benchmark_nonuniformffts(Z, CPU(), Ns, Nps; σ, m, use_atomics = true)
-    end
-
-    # CuFINUFFT
-    params_cufinufft = (;
-        gpu_sort = 1,    # needed for good performance
-        gpu_kerevalmeth = 1,  # setting it to 0 (direct evaluation) doesn't make a big difference?
-        tol = 1e-6,
-    )
-
-    if gpu
-        gpu_method = 1  # global memory (GM-sort)
-        @info "Running CuFINUFFT benchmark" Z=Z backend=CUDABackend() gpu_method=gpu_method
-        flush(stdout)
-        run_benchmark_cufinufft(Z, Ns, Nps; gpu_method, params_cufinufft...)
-
-        gpu_method = 2  # shared memory (SM)
-        @info "Running CuFINUFFT benchmark" Z=Z backend=CUDABackend() gpu_method=gpu_method
-        flush(stdout)
-        run_benchmark_cufinufft(Z, Ns, Nps; gpu_method, params_cufinufft...)
     end
 
     # FINUFFT CPU
