598daf2c5e10cdaee3ddd319561255ffa913846a
Juan Ignacio Polanco <juan-ignacio.polanco@cnrs.fr>
2026-01-23 13:19:15 +0100

    Update benchmarks; include OpenCLBackend

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,43 +5,59 @@ using NonuniformFFTs
 using FFTW: FFTW
 using Random: Xoshiro
 using BenchmarkTools
+using OpenCL, pocl_jll
+using Adapt: adapt
+using KernelAbstractions: KernelAbstractions as KA
 
 using ThreadPinning
 pinthreads(:cores)
 threadinfo()
 
-# Create benchmark suite for AirspeedVelocity.jl (benchmarks on github CI)
-const SUITE = BenchmarkGroup()
+# Print OpenCL information
+OpenCL.versioninfo()
+@show cl.platform()
+@show cl.device()
 
-function setup_cpu_benchmark(::Type{Z}, Ns::Dims, Np; σ, m, fftw_flags = FFTW.ESTIMATE, kws...) where {Z}
+function setup_benchmark(::Type{Z}, Ns::Dims, Np; backend = CPU(), σ, m, fftw_flags = FFTW.ESTIMATE, kws...) where {Z}
     D = length(Ns)
     T = real(Z)
     rng = Xoshiro(42)
-    xp = ntuple(D) do _
+    xp_cpu = ntuple(D) do _
         randn(rng, T, Np)
     end
-    vp = randn(rng, Z, Np)
-    backend = CPU()
+    vp_cpu = randn(rng, Z, Np)
+    xp = adapt(backend, xp_cpu)
+    vp = adapt(backend, vp_cpu)
     p = PlanNUFFT(Z, Ns; backend, σ, m, fftw_flags, kws...)
-    ûs = similar(vp, complex(T), size(p))
-    (; p, xp, vp, ûs,)
+    ûs_cpu = randn(rng, complex(T), size(p))
+    ûs = adapt(backend, ûs_cpu)
+    (; backend, p, xp, vp, ûs, vp_out = similar(vp), ûs_out = similar(ûs))
 end
 
-function run_cpu_benchmark_type1(data)
-    (; p, xp, vp, ûs,) = data
+function run_type1(data)
+    (; backend, p, xp, vp, ûs_out,) = data
     set_points!(p, xp)
-    exec_type1!(ûs, p, vp)
-    nothing
+    exec_type1!(ûs_out, p, vp)
+    KA.synchronize(backend)
+    ûs_out
 end
 
-function run_cpu_benchmark_type2(data)
-    (; p, xp, vp, ûs,) = data
+function run_type2(data)
+    (; backend, p, xp, vp_out, ûs,) = data
     set_points!(p, xp)
-    exec_type2!(vp, p, ûs)
-    nothing
+    exec_type2!(vp_out, p, ûs)
+    KA.synchronize(backend)
+    vp_out
 end
 
-## Run benchmarks
+get_subsuite(suite, ::Type{Z}; backend, Ns, ρ, σ, m) where {Z} = suite[string(typeof(backend))][string(Z)]["Ns = $Ns"]["ρ = $ρ"]["σ = $σ"]["m = $m"]
+
+## Define benchmarks
+
+backends = [
+    CPU(),
+    OpenCLBackend(),
+]
 
 Ns = (128, 128, 128)
 Np = prod(Ns)
@@ -49,19 +65,40 @@ Np = prod(Ns)
 m = HalfSupport(4)
 
 ρ = Np / prod(Ns)
-case = "CPU: Ns = $Ns, ρ = $ρ, σ = $σ, m = $m"
 
-for Z in (Float64, ComplexF64)
-    for (key_atomics, use_atomics) in ("atomics" => true, "no atomics" => false)
-        local data = setup_cpu_benchmark(Z, Ns, Np; σ, m, use_atomics)
-        SUITE[case][key_atomics]["$Z Type 1"] = @benchmarkable run_cpu_benchmark_type1($data)
-        SUITE[case][key_atomics]["$Z Type 2"] = @benchmarkable run_cpu_benchmark_type2($data)
+# Create benchmark suite for AirspeedVelocity.jl (benchmarks on github CI)
+const SUITE = BenchmarkGroup()
+
+for backend in backends, Z in (Float64, ComplexF64)
+    sub = get_subsuite(SUITE, Z; backend, Ns, ρ, σ, m)
+    if backend isa CPU
+        for (key_atomics, use_atomics) in ("atomics" => true, "no atomics" => false)
+            local data = setup_benchmark(Z, Ns, Np; backend, σ, m, use_atomics)
+            sub[key_atomics]["Type 1"] = @benchmarkable run_type1($data)
+            sub[key_atomics]["Type 2"] = @benchmarkable run_type2($data)
+        end
+    else
+        for gpu_method in (:global_memory, :shared_memory)
+            local data = setup_benchmark(Z, Ns, Np; backend, σ, m, gpu_method)
+            sub[gpu_method]["Type 1"] = @benchmarkable run_type1($data)
+            sub[gpu_method]["Type 2"] = @benchmarkable run_type2($data)
+        end
     end
 end
 
+# Check that CPU and OpenCL backends give the same results.
+data = setup_benchmark(Float64, Ns, Np; backend = CPU(), σ, m)
+us_cpu = run_type1(data)
+vp_cpu = run_type2(data)
+
+data = setup_benchmark(Float64, Ns, Np; backend = OpenCLBackend(), σ, m)
+us_opencl = run_type1(data)
+vp_opencl = run_type2(data)
+
+@show isapprox(us_cpu, Array(us_opencl); rtol = 1e-14)
+@show isapprox(vp_cpu, Array(vp_opencl); rtol = 1e-14)
+
 # Example interactive usage:
 # @time tune!(SUITE)
-# results = run(SUITE[case]["atomics"]["Float64 Type 1"]; verbose = true, seconds = 2)
-# results = run(SUITE[case]["atomics"]["ComplexF64 Type 1"]; verbose = true, seconds = 2)
-# results = run(SUITE[case]["no atomics"]["Float64 Type 1"]; verbose = true, seconds = 2)
-# results = run(SUITE[case]["no atomics"]["ComplexF64 Type 1"]; verbose = true, seconds = 2)
+# results_cpu = run(get_subsuite(SUITE, Float64; backend = CPU(), Ns, ρ, σ, m))
+# results_opencl = run(get_subsuite(SUITE, Float64; backend = OpenCLBackend(), Ns, ρ, σ, m))
